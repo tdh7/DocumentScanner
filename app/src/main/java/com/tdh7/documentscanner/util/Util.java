8440a36cd79bddbaca4ed527118d74b90314d442
dtrung98 <trung17dinhle@gmail.com>
2019-10-13 17:34:03 +0700

    --

--- app/src/main/java/com/tdh7/documentscanner/util/Util.java
+++ app/src/main/java/com/tdh7/documentscanner/util/Util.java
@@ -57,8 +57,7 @@ public final class Util {
                 matrix, false);
     }
 
-    public static Bitmap rotateBitmap(Bitmap source, float angle)
-    {
+    public static Bitmap rotateBitmap(Bitmap source, float angle) {
         Matrix matrix = new Matrix();
         matrix.postRotate(angle);
         return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
